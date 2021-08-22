7ac4da553caff625219c4fe36280868d6efc90b7
Muhammad-Bin-Ali <muhammad_binali@yahoo.com>
2021-08-22 09:17:09 -0400

    final commit

--- public/app.js
+++ public/app.js
@@ -3,6 +3,11 @@ $(".vendor-detail-wrapper").fadeOut(0);
 $(".profile-div").fadeOut(0);
 $(".map-page").fadeOut(0);
 $(".create-div").fadeOut(0);
+$(".email-svg").hide();
+$(".phone-svg").hide();
+
+const Locations = [];
+
 
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
@@ -20,7 +25,23 @@ document.addEventListener("DOMContentLoaded", event => {
             $('.signOut').hide();
         }
     })
+    getVendors()
 }); 
+
+async function getVendors() {
+    const vendLoc = [];
+    await firebase.firestore().collection('vendors').get()
+      .then(querySnapshot => {
+        querySnapshot.docs.forEach(doc => {
+        vendLoc.push(doc.data());
+      });
+    });
+    for (var i = 0; i < vendLoc.length; i++){
+      var Location = vendLoc[i].Location;
+      Locations.push(Location);
+    }
+    return vendLoc;
+  }
 
 function googleSignIn() {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -84,15 +105,14 @@ document.querySelector('.save-btn').addEventListener('click', () => {
 })
 
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 12,
-      });
+function initMap(Location, Range) {
+    var Gigs = [];
+
     const service = new google.maps.DistanceMatrixService(); // instantiate Distance Matrix servic
-    var origin1 = "575 Willmott Crescent, Milton, ON, CANADA";
-    var destination1 = "233 S Wacker Dr, Chicago, IL, USA";
-    const matrixOptions = {
+    var origin1 = Location;
+    for (var i = 0; i < Locations.length; i++){
+      var destination1 = Locations[i];
+      const matrixOptions = {
         origins: [origin1], // technician locations
         destinations: [destination1], // customer address
         travelMode: 'DRIVING',
@@ -103,23 +123,85 @@ function initMap() {
 
       // Callback function used to process Distance Matrix response
       function callback(response, status) {
-        if (status == 'OK') {
-          var origins = response.originAddresses;
-          var destinations = response.destinationAddresses;
-      
-          for (var i = 0; i < origins.length; i++) {
-            var results = response.rows[i].elements;
-            for (var j = 0; j < results.length; j++) {
-              var element = results[j];
-              var distance = element.distance.text;
-              var from = origins[i];
-              var to = destinations[j];
-              console.log(distance);
+        if (status === 'OK') {
+            var origins = response.originAddresses;
+            var destinations = response.destinationAddresses;
+            for (var i = 0; i < origins.length; i++) {
+                var results = response.rows[i].elements;
+                for (var j = 0; j < results.length; j++) {
+                var element = results[j];
+                var distance = element.distance.text;
+                var from = origins[i];
+                var to = destinations[j];
+              
+                if (parseFloat(distance) < Range) {
+                    Gigs.push(to);
+                }
             }
           }
         }
       }
+    }
+
+    return Gigs;
 }
+
+
+async function createEntries(Gigs) {
+    const db = firebase.firestore();
+    if (Gigs) {
+        document.querySelector('.results').innerText = (Gigs.length) + " Results";
+    }
+  
+    for (var i = 0; i < Gigs.length; i++) {
+        setTimeout(() => {})
+        var address = Gigs[i];
+        var vendLoc = [];
+        await db.collection('vendors').get()
+        .then(querySnapshot => {
+          querySnapshot.docs.forEach(doc => {
+          vendLoc.push(doc.data());
+        })});
+        console.log(vendLoc);
+        console.log(Gigs)   
+        for (var i = 0; i < vendLoc.length ; i++) {
+            for (var j = 0; j < Gigs.length; j++) {
+                if (Gigs[j].includes(vendLoc[i].Location)) {
+                    console.log('works')
+                    $('.results-container').append(`
+                    <div class='result-entry'>
+                    <img class='result-img' src='https://images.unsplash.com/photo-1524230659092-07f99a75c013?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'>
+                    <h1 class='vendor-name'>${vendLoc[i].vendorName}</h1>
+                    <h3 class='location'>${vendLoc[i].Location}</h3>
+                        </div>`)
+                          
+                }
+            }
+
+           
+             }
+
+        }
+
+    fillDetails();
+}
+
+function fillDetails() {
+    var elements = document.getElementsByClassName('result-entry');
+    Array.from(elements).forEach((elem) => {
+        elem.addEventListener('click', () => {
+            console.log('works')
+            $(".email-svg").show();
+            $(".phone-svg").show();
+            document.querySelector('.details-title').innerText = "Squidward Tentacles";
+            document.querySelector('.details-location').innerText = "100 Queens Park, Toronto, ON ";
+            document.querySelector('.phone-number').innerText = "365-776-7202";
+            document.querySelector('.email').innerText = "squidward@gmail.com";
+            document.querySelector('.vendor-desc').innerText = "Looking for Jazz fingerstyle guitarist. Drummers also welcome and drums and PA system provided in-house."
+        })
+    })
+}
+
 
 document.querySelector('#create').addEventListener('click', () => {
     console.log('works')    
@@ -138,3 +220,19 @@ document.querySelector('#create').addEventListener('click', () => {
         document.querySelector('#email-input-vendor').value = "";
     });
 })
+
+
+function addEvent() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 12,
+      });
+    document.querySelector('.search-button').addEventListener('click', () => {
+        var address = document.querySelector('.location-input').value;
+        var range = parseInt(document.querySelector('.distance-input').value)
+        var Gigs = initMap(address, range);
+        setTimeout(() => {
+            createEntries(Gigs);
+        }, 1000);
+    })
+}
