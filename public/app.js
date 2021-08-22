e5d80c711a0c9cf7331a517351506d157485cf28
Muhammad-Bin-Ali <muhammad_binali@yahoo.com>
2021-08-22 01:17:42 -0400

    god bless

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
-// $(".profile-div").fadeOut(0);
-// $(".vendor-detail-wrapper").fadeOut(0);
+$(".profile-div").fadeOut(0);
+$(".vendor-detail-wrapper").fadeOut(0);
+$(".profile-div").fadeOut(0);
 
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
@@ -11,7 +12,6 @@ document.addEventListener("DOMContentLoaded", event => {
             checkIfExistsorCreateProfile(user)
             getUserProfilePic(user);
         }
-
         else {
             $('.googleAuth').show();
         }
@@ -58,9 +58,30 @@ function getUserProfilePic(user) {
 
 let map;
 
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 12,
+//   });
+// }
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    const service = new google.maps.DistanceMatrixService(); // instantiate Distance Matrix service
+      const matrixOptions = {
+        origins: ["41.8848274,-87.6320859", "41.878729,-87.6301087", "41.8855277,-87.6440611"], // technician locations
+        destinations: ["233 S Wacker Dr, Chicago, IL 60606"], // customer address
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+      };
+      // Call Distance Matrix service
+      service.getDistanceMatrix(matrixOptions, callback);
+
+      // Callback function used to process Distance Matrix response
+      function callback(response, status) {
+        if (status !== "OK") {
+          alert("Error with distance matrix");
+          return;
+        }
+        console.log(response);        
+      }
 }
